e7f14cea4464d3f3c17ba07bcab57a9011d9ae46
JanoLampaertHoGent <61159482+JanoLampaertHoGent@users.noreply.github.com>
2020-04-15 14:26:59 +0200

    disable REMOVEDB

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const routes = require("./routes");
 const PORT = 8081;
-const REMOVEDB = true;
+const REMOVEDB = false;
 const path = require("path");
 
 // Initialize Database
